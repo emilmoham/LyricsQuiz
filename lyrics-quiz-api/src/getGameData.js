20c48c9261cb8943d69eaefef19c819a11f8db79
Emil Mohammed <emilmoham@gmail.com>
2023-10-07 10:13:04 -0400

    Added back end api to download lyrics

---
+++ lyrics-quiz-api/src/getGameData.js
@@ -0,0 +1,54 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const extractTitle = (fullHTML) => {
+    const $ = cheerio.load(fullHTML);
+    return $('title').html().split('|')[0].trim();
+}
+
+const extractLyrics = (fullHTML) => {
+    const $ = cheerio.load(fullHTML);
+
+    let lyrics = $('div.lyrics').text()
+
+        /* genius.org serves two DOMs for its lyrics pages, the below
+           scrapes the second style (that does not contain a lyrics div) */
+
+        if(!lyrics){
+            $('[class^=Lyrics__Container]').each((i, el) => {
+                const html = $(el).html()
+                const lined = html.replace(/<br\s*[\/]?>/gi, "\n")
+                const stripped = lined.replace(/<[^>]+>/ig, '')
+                const trimmed = stripped.trim()
+                lyrics += trimmed
+            })
+        }
+        if(!lyrics || fullHTML.includes('Lyrics for this song have yet to be')) {
+            console.log('Failed to capture lyrics or none present')
+            if(fullHTML.includes('Burrr!'))
+                console.log('could not find url ', url)
+            return null
+        }
+    return lyrics;
+}
+
+const getGameData = async (resource) => {
+    let gameData = {}            
+    gameData.link = `https://genius.com/${resource}`;
+
+    const res = await axios.get(gameData.link);
+
+    try {
+        const fullHTML = res.data;
+        gameData.title = extractTitle(fullHTML)
+        gameData.lyrics = extractLyrics(fullHTML);
+    } catch (e) {
+        console.log(e);
+        gameData.title = null;
+        gameData.lyrics = null;
+    }
+
+    return gameData;
+}
+
+module.exports =  getGameData;
---
+++ lyrics-quiz-api/src/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+
+const getGameData = require('./getGameData');
+
+const port = 8001;
+
+const app = express();
+
+app.use(cors());
+
+app.get('/getGameData/:title', async function (req, res, next) {
+    const title = req.params.title;
+
+    const gameData = await getGameData(title);
+
+    res.json(gameData);
+  })
+
+app.listen(port, function () {
+    console.log(`CORS-enabled web server listening on port ${port}`)
+  });
