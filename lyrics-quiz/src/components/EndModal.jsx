cae75fb6819f93addbc743741668fbe05c484275
Emil Mohammed <emilmoham@gmail.com>
2023-10-13 21:48:15 -0400

    Implemented End Screen buttons

--- lyrics-quiz/src/components/EndModal.jsx
+++ lyrics-quiz/src/components/EndModal.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ReactModal from 'react-modal';
+import { useNavigate } from 'react-router-dom';
 
 const EndModal = (props) => {
     const {
@@ -7,6 +8,8 @@ const EndModal = (props) => {
         showModal,
         onCloseModal
     } = props;
+
+    const navigate = useNavigate();
 
     const calculateTotalGameTime = () => {
         let totalGameSeconds = 0;
@@ -17,10 +20,41 @@ const EndModal = (props) => {
         return {totalGameSeconds: totalGameSeconds, minutes: minutes, seconds: seconds}
     }
 
-    const renderElapsedGameTime = () => {
+    const getTimeString = () => {
         const elapsedTime = calculateTotalGameTime();
-        return <h3>{elapsedTime.minutes}:{elapsedTime.seconds.toString().padStart(2,'0')}</h3> 
+        return `${elapsedTime.minutes}:${elapsedTime.seconds.toString().padStart(2,'0')}`
     }
+
+    const renderElapsedGameTime = () => {
+        return <h3>{getTimeString()}</h3> 
+    }
+
+    const getScoreString = () => {
+        return `${gameData.currentScore}/${gameData.maxPossibleScore}`
+    }
+
+    const getCurrentUrl = () => {
+        let url = window.location.href;
+        url = url.replace('https://','');
+        return url;
+    }
+
+    const onClickNewSong = (e) => {
+        e.preventDefault();
+        navigate('/')
+    }
+
+    const onClickRetry = (e) => {
+        e.preventDefault();
+        navigate(0)
+    }
+
+    const onClickShare = (e) => {
+        e.preventDefault();
+        navigator.clipboard.writeText(`🎶 ${gameData.title} - ${getScoreString()} in ${getTimeString()} 🎶\n${getCurrentUrl()}`)
+    }
+
+
 
     return (
     <ReactModal 
@@ -30,14 +64,14 @@ const EndModal = (props) => {
         className={'modal-container'}>
         <button className={'close-modal-button'} onClick={onCloseModal}>X</button>
         <h1>Final Score:</h1>
-        <h3>{gameData.currentScore}/{gameData.maxPossibleScore}</h3>
+        <h3>{getScoreString()}</h3>
         <div>in</div>
         {renderElapsedGameTime()}
         <div className='spacer'></div>
-        <button className='menu-button share-button'>Copy Results</button>
+        <button className='menu-button share-button' onClick={onClickShare}>Copy Results</button>
         <div className='end-navigation'>
-            <button className='menu-button end-control'>New Song</button>
-            <button className='menu-button end-control'>Retry</button>
+            <button className='menu-button end-control' onClick={onClickNewSong}>New Song</button>
+            <button className='menu-button end-control' onClick={onClickRetry}>Retry</button>
         </div>
     </ReactModal>
     );
