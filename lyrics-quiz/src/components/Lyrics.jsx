5fe7b6d503ddfcdb22687c63e8f0d194324d91b4
Emil Mohammed <emilmoham@gmail.com>
2023-10-13 21:20:21 -0400

    Fixed missing key warning

--- lyrics-quiz/src/components/Lyrics.jsx
+++ lyrics-quiz/src/components/Lyrics.jsx
@@ -7,11 +7,19 @@ const Lyrics = (props) => {
         gameData,
     } = props;
 
+    const renderSectionHeader = (words) => {
+      let output = ''
+      for(let i = 0; i < words.length; i++){
+        output += words[i].revealedText + ' ';
+      }
+      return output;
+    }
+
     return (
     <div className='lyrics-container'>
         {gameData.lyrics.map((line, index) => {
           if (line.isSectionHeader) {
-            return (<h4>{line.words.map((word) => {return word.revealedText + ' '})}</h4>)
+            return (<h4 key={index}>{renderSectionHeader(line.words)}</h4>)
           }
           return (
             <LineContainer key={index} line={line} revealedWords={gameData.answerMap} isGameOver={gameData.isGameOver} />
