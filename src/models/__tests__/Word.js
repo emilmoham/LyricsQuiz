b669cac8722eb294b7879d49a66624134e210e33
Emil Mohammed <emilmoham@gmail.com>
2023-10-17 21:18:24 -0400

    Separators and Section Headers now covered

--- src/models/__tests__/Word.js
+++ src/models/__tests__/Word.js
@@ -6,7 +6,42 @@ test.each([
     ['(Parenthetical)', { revealedText: '(Parenthetical)', hiddenText: '(_____________)', logicalText: 'parenthetical', separator: '' }],
     ['lingüístico', { revealedText: 'lingüístico', hiddenText: '___________', logicalText: 'linguistico', separator: '' }],
     ['365', { revealedText: '365', hiddenText: '___', logicalText: '365', separator: '' }],
-])('Word', (input, output) => {
-    let word = Word(input, '', false);
-    expect(word).toEqual(output);
+])('Word_Blank_Separator', (input, output) => {
+    expect(Word(input, '', false)).toEqual(output);
+});
+
+// Non section headers, with separation charachters
+test.each([
+    [
+        { text: "Word", separator: '-' }, 
+        { revealedText: 'Word', hiddenText: '____', logicalText: 'word', separator:'-' }
+    ],
+    [
+        { text: "(Parenthetical)", separator: ' ' }, 
+        { revealedText: '(Parenthetical)', hiddenText: '(_____________)', logicalText: 'parenthetical', separator:' ' }
+    ],
+    [
+        { text: "lingüístico", separator: '-' },
+        { revealedText: 'lingüístico', hiddenText: '___________', logicalText: 'linguistico', separator:'-' }
+    ],
+    [
+        { text: "365", separator: '-' },
+        { revealedText: '365', hiddenText: '___', logicalText: '365', separator:'-' }
+    ],
+])('Word_With_Separator', (input, output) => {
+    expect(Word(input.text, input.separator, false)).toEqual(output);
+})
+
+// Section Headers
+test.each([
+    [
+        '[Word]', 
+        { revealedText: '[Word]', hiddenText: '[Word]', logicalText: '[Word]', separator: '' }
+    ],
+    [
+        '[Multiple Words]', 
+        { revealedText: '[Multiple Words]', hiddenText: '[Multiple Words]', logicalText: '[Multiple Words]', separator: '' }
+    ],
+])('Word_Section_Header', (input, output) => {
+    expect(Word(input, '', true)).toEqual(output);
 });
