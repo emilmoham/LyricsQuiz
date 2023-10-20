b72e69b3a8f571ccd7c00ed5e330e68663c46344
Emil Mohammed <emilmoham@gmail.com>
2023-10-19 21:30:24 -0400

    Initalizing game data

--- src/hooks/useGameData.js
+++ src/hooks/useGameData.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { Line } from '../models/Line';
 import axios from 'axios';
 
-function useGameData(props) {
+function useGameData() {
     const [title, setTitle] = useState('Loading...123');
     const [lyrics, setLyrics] = useState([]);
     const [answerMap, setAnswerMap] = useState(new Map());
@@ -17,10 +17,23 @@ function useGameData(props) {
 
     const [allowedGameSeconds, setAllowedGameSeconds] = useState(480);
 
-    const { song } = props;
-
     function parseLyrics(rawLyrics) {
-      return rawLyrics.split(/\n/g).map((rawLine) => Line(rawLine));
+      return rawLyrics.split(/\n/g).map((rawLine) => Line(rawLine)); // TODO remove blank lines
+    }
+
+    function initializeAnswerMap(lyricsSet) {
+      const myMap = new Map()
+
+      lyricsSet.map((line) => {
+        if (!line.isSectionHeader) {
+          line.words.map((word) => {
+            if (!myMap.has(word.logicalText))
+              myMap.set(word.logicalText, false);
+          })
+        }
+      });
+
+      return myMap;
     }
 
     function startQuiz() {
@@ -34,24 +47,30 @@ function useGameData(props) {
     }
 
     function checkAnswer() {
-      return true;
+      return true; // TODO
     }
 
     function loadSong(song) {
       console.log(song);
-      if (song !== undefined)
-      {
-        axios.get(`${process.env.REACT_APP_LYRICS_QUIZ_API_HOST}/getGameData/${song}`).then((response) => {
-            setTitle(response.data.title);
-            setLyrics(parseLyrics(response.data.lyrics));
-          }, (reason) => {
-            setTitle('Error');
-            //setLyrics(parseLyrics(response.data.lyrics));
+      if (song === undefined || song === null)
+        return;
+      axios.get(`${process.env.REACT_APP_LYRICS_QUIZ_API_HOST}/getGameData/${song}`).then((response) => {
+        setTitle(response.data.title);
 
-            // TODO Log Error properly
-            console.log(reason.message);
-          });
-      }
+        const lyricsSet = parseLyrics(response.data.lyrics);
+        const initialAnswerMap = initializeAnswerMap(lyricsSet);
+        
+        setLyrics(lyricsSet);
+        setAnswerMap(initialAnswerMap);
+        setMaxPossibleScore(answerMap.size)
+      }, (reason) => {
+        setTitle('Error');
+        //setLyrics(parseLyrics(response.data.lyrics));
+
+        // TODO Log Error properly
+        console.log(reason.message);
+      });
+
     }
 
     return {
